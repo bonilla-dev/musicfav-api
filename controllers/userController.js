67c38f97879ef9097732a0e0c224ec74a0d1df2e
Alvaro Bonilla Garcia <bonilladeveloper@gmail.com>
2021-06-14 10:44:36 +0200

    Fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -104,7 +104,7 @@ router.route('/users/:userId')
     }
   })
 router.route('/users/:userId/youtube')
-  .get(onlyRegisteredAccess, (req, res, next) => {
+  .get((req, res, next) => {
     const userId = req.params.userId
     req.app.set('userId', userId)
     passport.authenticate('youtube')
@@ -174,7 +174,7 @@ router.route('/youtube/callback')
   })
 
 router.route('/users/:userId/spotify')
-  .get(onlyRegisteredAccess,(req, res, next) => {
+  .get((req, res, next) => {
     const userId = req.params.userId
     req.app.set('userId', userId)
     passport.authenticate('spotify', {scope: ['user-read-email', 'user-read-private', 'user-library-read'],
